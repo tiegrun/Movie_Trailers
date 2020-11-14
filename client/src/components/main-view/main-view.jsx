6f93e6e17bf343a7c601c7f861c1aeac37d17495
tiegrun <tiko3020@gmail.com>
2020-11-14 04:09:35 -0500

    react

---
+++ client/src/components/main-view/main-view.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import axios from 'axios';
+
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
+
+  constructor() {
+    // Call the superclass constructor
+    // so React can initialize it
+    super();
+
+    // Initialize the state to an empty object so we can destructure it later
+    this.state = {
+      movies: null,
+      selectedMovie: null
+    };
+  }
+
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios.get('https://tiegrun-movie-trailers.herokuapp.com/movies')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view"/>;
+
+    return (
+     <div className="main-view">
+      {selectedMovie
+         ? <MovieView movie={selectedMovie}/>
+         : movies.map(movie => (
+           <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+         ))
+      }
+     </div>
+    );
+  }
+}
