ffb3ca4f129e8b921cc81a47eefa9379920b8e62
tiegrun <tiko3020@gmail.com>
2020-12-12 02:42:10 -0500

    fixed update password code

--- index.js
+++ index.js
@@ -149,10 +149,12 @@ app.post('/users',
 });
 
 app.put('/users/:Username', passport.authenticate('jwt', {session: false}), (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: hashedPassword,
+      // Password: req.body.Password,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }
